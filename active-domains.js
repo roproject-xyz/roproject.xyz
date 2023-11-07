569ce85d7cfd679a0dab5197eeb141761d9a3963
Marcus Harvey <flvffywvffy@gmail.com>
2023-11-07 15:49:54 +0000

    Create active-domains.js

---
+++ active-domains.js
@@ -0,0 +1,11 @@
+/*
+
+  **** ACTIVE DOMAINS ****
+  This is where all active domains are. Please add your name in alphabetical order and
+  not at the bottom.
+
+  Please look at the reserved domains first, as you can't use these. I can and will reject
+  any domain that looks like a scam or impersonation, but if you find someone doing this,
+  please email me here: report@roproject.xyz
+  
+*/
